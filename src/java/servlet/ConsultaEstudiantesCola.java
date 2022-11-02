2a1d97512881ed84b8b97832a84a8936e0edac91
JosueEmilian <juyul@miumg.edu.gt>
2022-11-02 10:53:02 -0600

    f

--- src/java/servlet/ConsultaEstudiantesCola.java
+++ src/java/servlet/ConsultaEstudiantesCola.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "ConsultaEstudiantesCola", urlPatterns = {"/ConsultaEstudiantesCola"})
 
 public class ConsultaEstudiantesCola extends HttpServlet {
 
